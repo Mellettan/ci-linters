52c1bb00d80cd64e172b31331a113c23bc9d7c7a
Mellettan <roma23470@gmail.com>
2024-07-10 02:02:34 +0300

    for black

--- src/database.py
+++ src/database.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)  # noqa
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
 session = async_session()
 Base = declarative_base()
