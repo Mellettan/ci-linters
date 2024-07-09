218c4dde14c9833c2dc8f7d503850e56cd369aef
Mellettan <roma23470@gmail.com>
2024-07-10 02:03:36 +0300

    for black

--- src/database.py
+++ src/database.py
@@ -7,6 +7,6 @@ DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
-)
+)  # noqa
 session = async_session()
 Base = declarative_base()
